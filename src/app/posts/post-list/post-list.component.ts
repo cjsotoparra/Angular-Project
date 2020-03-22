43bab9e41959113b61ebdf3e7890916364c0289c
christian soto <cjsotoparra@email.arizona.edu>
2020-03-22 02:22:45 -0700

    Added mongodb and mongoose to our app

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -21,7 +21,11 @@ export class PostListComponent implements OnInit, OnDestroy{
 		this.postsSub = this.postsService.getPostUpdateListener().subscribe((posts: Post[]) => {
 			this.posts = posts;
 		});
-	}
+  }
+
+  onDelete(postId: string){
+    this.postsService.deletePost(postId);
+  }
 
 	ngOnDestroy () {
 		this.postsSub.unsubscribe();
