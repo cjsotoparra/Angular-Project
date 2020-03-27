5f51a4cbc571bdad6aae6ca8c32feef68caef5db
christian soto <cjsotoparra@email.arizona.edu>
2020-03-26 23:45:48 -0700

    Cleaned up backend routes

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -37,6 +37,10 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string){
+    return this.http.get<{_id: string, title: string, content: string }>("http://localhost:3000/api/posts/" + id);
+  }
+
   addPost(title: string, content: string) {
     const post: Post = { id: null, title: title, content: content };
     this.http
@@ -49,14 +53,16 @@ export class PostsService {
       });
   }
 
-  getPost(id: string){
-    return {...this.posts.find(p => p.id === id)};
-  }
-
   updatePost(id: string, title: string, content: string){
     const post: Post = {id: id, title: title, content: content };
     this.http.put("http://localhost:3000/api/posts/" + id, post)
-    .subscribe(response => console.log(response))
+    .subscribe(response => {
+      const updatedPosts = [...this.posts];
+      const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+      updatedPosts[oldPostIndex] = post;
+      this.posts = updatedPosts;
+      this.postsUpdated.next([...this.posts]);
+    });
   }
 
   deletePost(postId: string){
