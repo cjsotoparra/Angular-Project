66bb85cc6a9b63414783a05067ed48d6c1fd6fce
christian soto <cjsotoparra@email.arizona.edu>
2020-04-09 15:53:03 -0700

    Connected Angular to the backend

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http"
+
+import { AuthData } from "./auth-data.model"
+
+@Injectable({ providedIn: "root" })
+export class AuthService{
+	constructor(private http: HttpClient){}
+
+	createUser(email: string, password: string, username: string, birthdate: string){
+		const authData: AuthData = {email: email, password: password, username: username, birthdate: birthdate};
+		this.http.post("http://localhost:3000/api/user/signup", authData).subscribe(response => {
+			console.log(response);
+		});
+	}
+}
