7ad231f26799d5c2b60df21118a45d0826493628
christian soto <cjsotoparra@email.arizona.edu>
2020-04-10 19:18:08 -0700

    Cnnected the logout button to the authentication status

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,7 +8,7 @@ import { AuthService } from "../auth/auth.service"
 @Component({
 	selector: 'app-header',
 	templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	styleUrls: ['./header.component.css']
 })
 
 export class HeaderComponent implements OnInit, OnDestroy {
@@ -21,6 +21,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
 		this.authListenerSubs = this.authService.getAuthStatusListener().subscribe(isAuthenticated => {
 			this.userIsAuthenticated = isAuthenticated;
 		});
+	}
+
+	onLogout(){
+		this.authService.logout();	
 	}
 
 	ngOnDestroy() {
