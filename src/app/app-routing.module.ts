eca39f062a9ab0c7c1f317dac3dd7fc9f6250f7c
christian soto <cjsotoparra@email.arizona.edu>
2020-04-16 22:53:44 -0700

    Added a Reference to Model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,8 +16,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  	imports: [RouterModule.forRoot(routes)],
-  	exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 	providers: [AuthGuard]
 
 })
